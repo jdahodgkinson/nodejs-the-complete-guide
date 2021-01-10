31a1ca0044d94acadcf72b813a957738b445908f
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-01-10 18:23:56 +0000

    Completed assignment 3 and working through section 6

--- section6-dynamic-content-templating/routes/admin.js
+++ section6-dynamic-content-templating/routes/admin.js
@@ -5,13 +5,19 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product.pug', {docTitle: 'Add Product', path:'/admin/add-product'});
 });
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+    routes: router,
+    products: products
+};
+
