267cbf90f314dd6d62c9b45276a5cec0a7ebe055
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-01-18 16:57:47 +0000

    inited section13

---
+++ section13-mongoose/util/database.js
@@ -0,0 +1,32 @@
+const mongodb = require('mongodb');
+const password = require('./secrets');
+
+const uri = `mongodb+srv://jack:${ password }@cluster0.odzpe.mongodb.net/shop?retryWrites=true&w=majority`;
+
+const MongoClient = mongodb.MongoClient(uri, { useUnifiedTopology: true });
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect()
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
